053051ea9d39111a8ade37e952f07758ec941530
Noa Shachar <shachar.dela@gmail.com>
2025-01-10 17:55:01 +0200

    added route param to the company rating page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router'
+import { BrowserRouter, Routes, Route, Link } from 'react-router';
 import Home from './pages/HomePage/HomePage';
 import styles from './styles/App.module.css';
 import About from './pages/AboutPage/AboutPage';
-import projectLogo from './assets/bodyfriendly-logo.png'
+import projectLogo from './assets/bodyfriendly-logo.png';
 import Rating from './pages/RatingPage/RatingPage';
 import AddBrand from './pages/AddBrandPage/AddBrandPage';
 
@@ -14,16 +14,17 @@ function App() {
           <img src={projectLogo} alt="Logo" className={styles.appLogo} />
           <h1 title="bodyfriendly">BODYFRIENDLY</h1>
           <nav className={styles.appNav}>
-            <Link to="/" className={styles.appLink}>Home</Link>
+            <Link to="/" className={styles.appLink}>
+              Home
+            </Link>
           </nav>
         </header>
         <main className={styles.main}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/rating" element={<Rating />} />
+            <Route path="/rating/:companyID" element={<Rating />} />
             <Route path="/add-brand" element={<AddBrand />} />
-
           </Routes>
         </main>
         <footer className={styles.footer}>
