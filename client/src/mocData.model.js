da79a4382dff53ab0fb0e5d5bcdf3782ee03409a
Noa Shachar <shachar.dela@gmail.com>
2025-01-10 17:52:22 +0200

    made a moc for companies api

--- client/src/mocData.model.js
+++ client/src/mocData.model.js
@@ -2,38 +2,39 @@ import modelImage from './assets/model.jpeg';
 
 const mockData = [
   {
-    name: "Fashion Co.",
+    name: 'Fashion Co.',
     image: modelImage,
     rating: 50,
-    championNumber:1,
+    championNumber: 1,
     details: {
       userRating: 40,
-      sizeRange: "XS-XXL",
-      description: "Fashion Co. is a global leader in promoting positive body image through inclusive and diverse campaigns."
-    }
+      sizeRange: 'XS-XXL',
+      description:
+        'Fashion Co. is a global leader in promoting positive body image through inclusive and diverse campaigns.',
+    },
   },
   {
-    name: "Style Inc.",
+    name: 'Style Inc.',
     image: modelImage,
     rating: 40,
-    championNumber:2,
+    championNumber: 2,
     details: {
       userRating: 42,
-      sizeRange: "S-XL",
-      description: "Style Inc. focuses on creating inclusive clothing for all body types."
-    }
+      sizeRange: 'S-XL',
+      description: 'Style Inc. focuses on creating inclusive clothing for all body types.',
+    },
   },
   {
-    name: "Trendsetters Ltd.",
+    name: 'Trendsetters Ltd.',
     image: modelImage,
     rating: 38,
-    championNumber:3,
+    championNumber: 3,
     details: {
       userRating: 40,
-      sizeRange: "M-3XL",
-      description: "Trendsetters Ltd. is known for its campaigns that embrace diversity."
-    }
-  }
+      sizeRange: 'M-3XL',
+      description: 'Trendsetters Ltd. is known for its campaigns that embrace diversity.',
+    },
+  },
 ];
 
 export default mockData;
