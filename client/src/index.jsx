0b1e32e721f3f9c43634ffcc014bb63970c5091d
Noa Shachar <shachar.dela@gmail.com>
2025-01-10 17:52:59 +0200

    applied rect query

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/global.css'; 
+import './styles/global.css';
 import App from './App.jsx';
 import { DuckProvider } from './context/DuckContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DuckProvider>
-      <App /> 
-    </DuckProvider>
+    <QueryClientProvider client={queryClient}>
+      <DuckProvider>
+        <App />
+      </DuckProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
