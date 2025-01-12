184d592e23a1d9278b10cd5e3b56a6d66e1ecd27
Noa Shachar <shachar.dela@gmail.com>
2025-01-12 16:56:09 +0200

    changed names

--- client/src/mockBrands.model.ts
+++ client/src/mockBrands.model.ts
@@ -12,7 +12,7 @@ export type brand = {
 const Brands: brand[] = [
   {
     id: '1',
-    name: 'The Good Clothes brand',
+    name: 'Fashion Co.',
     ratingOverall: 5,
     ratingByCategory: [
       {
@@ -34,7 +34,7 @@ const Brands: brand[] = [
   },
   {
     id: '2',
-    name: 'EcoFashion Hub',
+    name: 'Style Inc',
     ratingOverall: 4,
     ratingByCategory: [
       {
@@ -56,7 +56,7 @@ const Brands: brand[] = [
   },
   {
     id: '3',
-    name: 'Inclusive Wear Co.',
+    name: 'Trendsetters Ltd.',
     ratingOverall: 4.5,
     ratingByCategory: [
       {
@@ -73,6 +73,72 @@ const Brands: brand[] = [
         category: 'Body-Positive Models',
         details: 'Focuses on self-confidence and diverse shapes',
         rating: 5,
+      },
+    ],
+  },
+  {
+    id: '4',
+    name: 'The Good Clothes brand.',
+    ratingOverall: 5,
+    ratingByCategory: [
+      {
+        category: 'Sizing Variety',
+        details: 'Has many sizes',
+        rating: 5,
+      },
+      {
+        category: 'Race Inclusivity',
+        details: 'Showcasing diverse people',
+        rating: 5,
+      },
+      {
+        category: 'Body-Positive Models',
+        details: 'Inclusive campaigns with body-positive messages',
+        rating: 5,
+      },
+    ],
+  },
+  {
+    id: '5',
+    name: 'EcoFashion Hub.',
+    ratingOverall: 4.5,
+    ratingByCategory: [
+      {
+        category: 'Sizing Variety',
+        details: 'Wide range of sizes',
+        rating: 4,
+      },
+      {
+        category: 'Race Inclusivity',
+        details: 'Strong race inclusivity across marketing',
+        rating: 5,
+      },
+      {
+        category: 'Body-Positive Models',
+        details: 'Focus on self-acceptance',
+        rating: 4,
+      },
+    ],
+  },
+  {
+    id: '6',
+    name: 'Inclusive Wear Co.',
+    ratingOverall: 4.7,
+    ratingByCategory: [
+      {
+        category: 'Sizing Variety',
+        details: 'Range of sizes from S to 3XL',
+        rating: 5,
+      },
+      {
+        category: 'Race Inclusivity',
+        details: 'Inclusive campaigns with a focus on diversity',
+        rating: 5,
+      },
+      {
+        category: 'Body-Positive Models',
+        details: 'Campaigns emphasize positive body image',
+        rating: 4,
       },
     ],
   },
