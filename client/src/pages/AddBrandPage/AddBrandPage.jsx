6248a18cd30ca259f6665ccadee011544820fbe0
Renana Turgeman <renanaturgeman058@gmail.com>
2025-01-12 11:30:07 +0200

    add brang page

--- client/src/pages/AddBrandPage/AddBrandPage.jsx
+++ client/src/pages/AddBrandPage/AddBrandPage.jsx
@@ -1,9 +1,67 @@
-// import styles from './AddBrand.module.css'
+import styles from './AddBrand.module.css'
+import FirstButton from '../../components/common/FirstButton/FirstButton';
+import { useState } from "react";
 
 const AddBrand = () => {
+  const [brandName, setBrandName] = useState('');
+  const [link, setLink] = useState('');
+  const [image, setImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    setImage(event.target.files[0]);
+  };
+
   return (
-    <div>
+    <div className={styles.formContainer}>
       <h1>להצעת רשת חדשה לדירוג</h1>
+      <h3>רוצים שנדרג את הרשת שלכם? כאן תוכלו להשאיר עליה פרטים </h3>
+
+      <form className={styles.form}>
+      
+      <div className={styles.formGroup}>
+        <label htmlFor="brandName" className={styles.label}>
+            שם הרשת:
+          </label>
+        <input type="text"
+               className={styles.input}
+               placeholder="שם הרשת" 
+               value={brandName}
+               onChange={(e)=>setBrandName(e.target.value)}
+        />
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="link" className={styles.label}>
+            לינק לאתר הרשת:
+          </label>
+          <input type="text" 
+                name="link" 
+                className={styles.input}
+                placeholder="לינק לאתר הרשת" 
+                value={link}
+                onChange={(e)=>setLink(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="imageUpload" className={styles.fileInput}>
+            העלאת תמונה
+          </label>
+          <input
+            id="imageUpload"
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            className={styles.hiddenInput}
+          />
+        </div>
+
+        <button type="submit" className={styles.button}>
+          שלח
+        </button>
+      </form>
+      
+      {image && <p>תמונה שהועלתה: {image.name}</p>}
     </div>
   );
 };
