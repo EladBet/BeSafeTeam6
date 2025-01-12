b442f3d5042a4c54b0fda93a85bf803b35e32dd5
Noa Shachar <shachar.dela@gmail.com>
2025-01-12 16:56:27 +0200

    added links to the brand id

--- client/src/components/Brand/Brand.jsx
+++ client/src/components/Brand/Brand.jsx
@@ -1,26 +1,29 @@
-import styles from './Brand.module.css'
+import styles from './Brand.module.css';
 import ChampionNumber from '../ChampionNumber/ChampionNumber';
 import Details from '../Details/Details';
 import useBrand from '../../context/BrandContext';
+import { Link } from 'react-router-dom';
 
 const Brand = () => {
-    const brand = useBrand();
-    
-    const handleClick = () => {
-        console.log("click")
-        // move to more deatils - TODO: create page
-    }
+  const brand = useBrand();
 
-    return(
-        <div className={styles.brand}>
-            <div className={styles.modelContainer}>
-                <ChampionNumber number={brand.championNumber} />
-                <img src={brand.image} alt="model" className={styles.modelImage} />
-                <Details className={styles.details}/>
-                <span className={styles.moreDetails} onClick={handleClick}>&lt;</span>
-            </div>
-        </div>
-    )
+  const handleClick = () => {
+    console.log('click');
+    // move to more deatils - TODO: create page
+  };
+
+  return (
+    <div className={styles.brand}>
+      <div className={styles.modelContainer}>
+        <ChampionNumber number={brand.championNumber} />
+        <img src={brand.image} alt="model" className={styles.modelImage} />
+        <Details className={styles.details} />
+        <Link to={`/rating/${brand.id}`} className={styles.moreDetails}>
+          <span onClick={handleClick}>&lt;</span>
+        </Link>{' '}
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
