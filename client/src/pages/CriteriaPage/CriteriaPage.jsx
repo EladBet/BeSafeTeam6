86a9cd52281518cc47c8205a163073976627e8b0
Noa Shachar <shachar.dela@gmail.com>
2025-01-14 19:22:48 +0200

    added criteria page and module

---
+++ client/src/pages/CriteriaPage/CriteriaPage.jsx
@@ -0,0 +1,30 @@
+import styles from './Criteria.module.css';
+
+function CriteriaPage() {
+  return (
+    <div className={styles.criteriaPage}>
+      <h1>?איך אנחנו מדרגות</h1>
+      <h3>:כל חברה שמבקשת לקבל חותמת מאיתנו נבדקת על פי קריטריונים מרכזיים</h3>
+      <br />
+      <br />
+      <ol>
+        <ul>
+          <span className={styles.boldText}>מגוון מידות</span> – האם החברה מציעה בגדים במידות מגוונות ומותאמות
+          לכל סוגי הגוף
+        </ul>
+        <ul>
+          <span className={styles.boldText}>ייצוג מגוון</span> – האם החברה משתמשת בדוגמניות ודוגמנים מכל גווני
+          העור, המידות, המגדרים
+        </ul>
+        <ul>
+          <span className={styles.boldText}>מסרים חיוביים</span> – האם הפרסום והקמפיינים של החברה מקדמים דימוי
+          גוף חיובי
+        </ul>
+      </ol>
+      .חברות שקיבלו דירוג גבוה יזכו לקבל מאיתנו חותמת מיוחדת ויוצגו ברשימת המותגים המומלצים שלנו
+      <p />
+    </div>
+  );
+}
+
+export default CriteriaPage;
