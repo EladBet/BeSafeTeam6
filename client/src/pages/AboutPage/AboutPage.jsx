924c26e78ca5e25c20f49162ec593f265c2c405d
Noa Shachar <shachar.dela@gmail.com>
2025-01-14 13:40:51 +0200

    added text

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -1,15 +1,28 @@
 import styles from './About.module.css';
-import logo from '../../assets/full-logo.jpg'
+import logo from '../../assets/full-logo.jpg';
 
-const About = () =>{
-    return(
-        <div className={styles.container}>
-            <div className={styles.about}>
-                <h1>אז מי אנחנו?</h1>
-            </div>
-            <img src={logo} alt="Logo" className={styles.logo}/>
-        </div>
-    )
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.about}>
+        <h1>?אז מי אנחנו</h1>
+        <p>
+          .האתר שלנו נוצר מתוך רצון לעודד ייצוג חיובי ומגוון של גופים בעולם האופנה
+          <br />
+          .אנחנו מאמינות בכך שלכל גוף, בכל גודל וצבע, מגיע לקבל מקום בעולם הזה
+          <br />
+          ,באמצעות האתר, אנחנו בודקות ומדרגות חברות אופנה על פי קריטריונים ברורים כמו מגוון מידות, <br />
+          .ייצוג של דוגמניות מכל הסוגים, ונכונות לקדם דימוי גוף בריא
+          <br />
+          ,המטרה שלנו היא להעלות מודעות
+          <br />
+          .לשפר את הסטנדרטים בעולם האופנה ולתמוך בחברות שמתחייבות לגיוון אמיתי
+          <br />
+        </p>
+      </div>
+      <img src={logo} alt="Logo" className={styles.logo} />
+    </div>
+  );
 };
 
 export default About;
