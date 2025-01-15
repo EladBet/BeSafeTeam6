07199bcee89a1883b1126657b1670e23fe16036d
Renana Turgeman <renanaturgeman058@gmail.com>
2025-01-15 22:43:56 +0200

    post rating

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 // import rubberDuckRoutes from './routes/rubberDucks.js'; // Import the routes
 import BrandRoutes from './routes/BrandRoutes.js'
+import ratingRouter from './routes/ratingRoutes.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,6 +23,9 @@ app.use(cors({
 
 // Use the routes file for all `/brands` routes
 app.use('/brands', BrandRoutes);
+
+// Use the routes file for all `/ratings` routes
+app.use('/ratings', ratingRouter); 
 
 // Start server
 const PORT = process.env.PORT;
