a7ae88c65ebf66a1c7eb24a0553d56ee8f014b5d
Noa Shachar <shachar.dela@gmail.com>
2025-01-15 14:10:04 +0200

    revert the commit to ducks

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import App from './App.jsx';
+import { DuckProvider } from './context/DuckContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <DuckProvider>
+        <App />
+      </DuckProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
