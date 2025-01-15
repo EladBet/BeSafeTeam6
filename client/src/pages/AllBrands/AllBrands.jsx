686107757bd096e1233d4bf7db990aa1a7693e2c
Renana Turgeman <renanaturgeman058@gmail.com>
2025-01-15 23:17:24 +0200

    all brands page

--- client/src/pages/AllBrands/AllBrands.jsx
+++ client/src/pages/AllBrands/AllBrands.jsx
@@ -3,30 +3,32 @@ import { fetchAllBrands } from '../../services/api';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import Stars from '../../components/Stars/Stars';
+import useApi from '../../hooks/useApi';
 
 const AllBrands = () => {
-  const { data, error, isPending } = useQuery({
-    queryKey: ['brands'],
-    queryFn: fetchAllBrands,
-    retry: false,
-  });
+  const { data, loading, error } = useApi("http://localhost:5000/brands");
 
-  if (isPending) {
-    return <i>Loading...</i>;
+  if (loading) {
+    return <p className={styles.loading}>Loading...</p>;
   }
 
   if (error) {
     return <p className={styles.error}>{error.message}</p>;
   }
 
+  if (!data || !data.brands || data.brands.length === 0) {
+    return <p className={styles.noData}>No brands found.</p>;
+  }
+
   return (
     <div className={styles.links}>
       <h1>Ratings by Brand</h1>
-      {data.map((brand) => (
-        <Link className={styles.link} key={brand._id} to={`/brands/${brand.brandName}`}>
-          <p>{brand.brandName}</p>
-          <Stars numStars={3.5} />
-        </Link>
+      {data.brands.map((brand) => (
+          <Link className={styles.link} key={brand._id} to={`/brands/${brand.name}`}>
+            <img src={brand.logo} alt={`${brand.name} logo`} className={styles.logo} />
+            <p>{brand.name}</p>
+            <Stars numStars={brand.score} />
+          </Link>
       ))}
     </div>
   );
