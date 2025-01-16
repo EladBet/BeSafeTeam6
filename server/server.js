319e77728255ec4a2d3025040df7cab66dbce2de
Noa Shachar <shachar.dela@gmail.com>
2025-01-16 03:09:37 +0200

    have cors expcet anything

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
+
 import dotenv from 'dotenv';
 // import rubberDuckRoutes from './routes/rubberDucks.js'; // Import the routes
-import BrandRoutes from './routes/BrandRoutes.js'
-import ratingRouter from './routes/ratingRoutes.js'
+import BrandRoutes from './routes/BrandRoutes.js';
+import ratingRouter from './routes/ratingRoutes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,18 +18,16 @@ const app = express();
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images'))); // Serve static images
 
-app.use(cors({
-  origin: process.env.CLIENT_URL
-}));
+app.use(cors());
 
 // Use the routes file for all `/brands` routes
 app.use('/brands', BrandRoutes);
 
 // Use the routes file for all `/ratings` routes
-app.use('/ratings', ratingRouter); 
+app.use('/ratings', ratingRouter);
 
 // Start server
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
