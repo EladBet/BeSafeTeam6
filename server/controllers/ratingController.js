e3289d5f6c88ff16afdec9409676c363073d1ec2
Renana Turgeman <renanaturgeman058@gmail.com>
2025-01-20 11:40:25 +0200

    brand table endpoint

--- server/controllers/ratingController.js
+++ server/controllers/ratingController.js
@@ -47,3 +47,32 @@ export const getBrandRates = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const getBrandTable = async (req, res) => {
+  const { brand } = req.params;
+
+  try {
+    const nearestRun = await database.collection('runs')
+      .find({
+        'brands.name': brand, // Filter for the brand name
+      })
+      .sort({ run_time: 1 }) // Sort by run_time (ascending)
+      .limit(1) // Get the nearest run
+      .toArray();
+
+    if (nearestRun.length === 0) {
+      return res.status(404).json({ error: 'No runs found for this brand' });
+    }
+
+    // Extract the matching brand details from the nearest run
+    const matchingBrand = nearestRun[0].brands.find(b => b.name === brand);
+
+    if (!matchingBrand) {
+      return res.status(404).json({ error: 'Brand not found in the run' });
+    }
+
+    res.status(200).json({ brand:matchingBrand });
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+};
